909c5a61a96d84045c8dcb38bef5bc37aae0e8ca
Muhammad Bilal <travelist.pk@gmail.com>
2025-07-15 04:27:04 +0500

    Update first.js

--- first.js
+++ first.js
@@ -1,7 +1,8 @@
-const product = {
-title: "Ball Pen",
-rating: 4,
-offer: 5,
-price: 270,
-}
-console.log('product');
+const profile = {
+username: "@shradhakhapra",
+isFollow: false,
+followers: 123,
+following: 123,
+I
+};
+console.log(typeof profile ["followers']);
